59ef3a3eaa5d1b7b394c54527f37f64b809bebec
Nikki972 <nik_vo2002@mail.ru>
2026-03-01 13:21:04 +0400

    Fix db exports: use named db export consistently

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,17 @@
 import { openDB } from 'idb'
 
-export const dbPromise = openDB('TutorAppDB', 1, {
+export const db = await openDB('tutor-db', 3, {
   upgrade(db) {
     if (!db.objectStoreNames.contains('students')) {
-      db.createObjectStore('students', { keyPath: 'id' })
+      db.createObjectStore('students', { keyPath: 'id', autoIncrement: true })
     }
+
     if (!db.objectStoreNames.contains('lessons')) {
-      db.createObjectStore('lessons', { keyPath: 'id' })
+      db.createObjectStore('lessons', { keyPath: 'id', autoIncrement: true })
+    }
+
+    if (!db.objectStoreNames.contains('settings')) {
+      db.createObjectStore('settings', { keyPath: 'key' })
     }
   },
 })
